0748dd839c2f6d270660dfc6411c3d6207eeb5a3
ekaadwar <eka.adwar@gmail.com>
2022-08-16 23:52:40 +0700

    membuat file index untuk image

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+export const alfamart = require("./logo/alfamart.png");
+export const bca = require("./logo/bca.png");
+export const bni = require("./logo/bni.png");
+export const bri = require("./logo/bri.png");
+export const completed = require("./logo/completed.png");
+export const logo = require("./logo/logo.png");
+export const logoAbu = require("./logo/logoAbu.png");
+export const logoNoName = require("./logo/logoNoName.png");
+export const mandiri = require("./logo/mandiri.png");
+export const panding = require("./logo/panding.png");
+export const sinarmas = require("./logo/sinarmas.png");
+export const smartfren = require("./logo/smartfren.png");
