4f81bd3019c1d8259719f2ced728b84cb8963d76
ekaadwar <eka.adwar@gmail.com>
2022-08-16 23:47:45 +0700

    menambahkan image yang dibutuhkan

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import store from "./src/redux/store";
 
 function App() {
   return (
