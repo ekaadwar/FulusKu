bee5163623a7dd3b40f60a83d055b4726b7793e6
ekaadwar <eka.adwar@gmail.com>
2022-08-19 07:33:33 +0700

    styling RegisterPhone screen

--- src/screens/RegisterPhone.js
+++ src/screens/RegisterPhone.js
@@ -5,16 +5,28 @@ import { GeneralStyle } from "../components/GeneralStyles";
 import { logoNoName } from "../assets";
 import MainInput from "../components/MainInput";
 import MainButton from "../components/MainButton";
+import SpaceHorizontal from "../components/SpaceHorizontal";
 
 const RegisterPhone = () => {
   return (
     <View style={[GeneralStyle.parentTop, GeneralStyle.headerPadding]}>
       <View style={GeneralStyle.container}>
         <Image source={logoNoName} />
-        <Text>Register your mobile number</Text>
-        <Text>This will be use as your FULUSKU account number</Text>
+        <SpaceHorizontal space={30} />
+        <Text style={GeneralStyle.titleScreen}>
+          Register your mobile number
+        </Text>
+        <SpaceHorizontal space={10} />
+        <Text style={GeneralStyle.mainText}>
+          This will be use as your FULUSKU account number
+        </Text>
+        <SpaceHorizontal space={30} />
         <MainInput placeholder="Enter Here" />
-        <Text>We will send verification code to this number</Text>
+        <SpaceHorizontal space={30} />
+        <Text style={GeneralStyle.mainText}>
+          We will send verification code to this number
+        </Text>
+        <SpaceHorizontal space={30} />
         <MainButton primary text="NEXT" />
       </View>
     </View>
